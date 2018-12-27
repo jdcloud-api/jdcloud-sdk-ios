07a168bec2ff815d69789617ef18847e7f410ed0
lishijun10 <lishijun1@jd.com>
2018-12-27 13:14:02 +0800

    ad vmtest code

---
+++ Tests/VmTests.swift
@@ -0,0 +1,28 @@
+import XCTest
+@testable import JDCloudSDKCore
+@testable import JDCloudSDKVm
+
+class VmTests: XCTestCase{
+    func testExample() {
+    }
+    
+    func testVmClient() throws{
+        
+        let semaphore = DispatchSemaphore(value: 0)
+        let credentials = Credential(accessKeyId: "your jdcloud ak", secretAccessKey: "your jdcloud sk");
+        let vmClient = VmJDCloudClient(credential: credentials)
+        let describeInstancesRequest = DescribeInstancesRequest(regionId: "cn-north-1");
+        GlobalConfig.debug = true
+        try vmClient.describeInstancesAsync(request: describeInstancesRequest) { (statusCode, requestResponse, error,resultString) in
+            print(statusCode)
+            print(requestResponse)
+            print(error)
+            semaphore.signal()
+        }
+        _ = semaphore.wait(timeout: DispatchTime.distantFuture)
+    }
+    
+    static var allTests = [
+        ("testVmClient", testVmClient),
+        ]
+}
