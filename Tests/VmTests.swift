8bdc950a3d4d9a66c7a33100493577eed5357e86
lishijun10 <lishijun1@jd.com>
2018-12-27 11:53:28 +0800

    update test file

--- Tests/VmTests.swift
+++ Tests/VmTests.swift
@@ -3,12 +3,6 @@ import XCTest
 @testable import JDCloudSDKVm
 
 class VmTests: XCTestCase{
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        //  XCTAssertEqual(Core.text, "Hello, World!")
-    }
     
     func testVmClient() throws{
         
