11622189e597c9f3bf204cf20ba4ff302da6a922
lishijun10 <lishijun1@jd.com>
2019-06-28 10:28:35 +0800

    remove build

--- build/JDCloudSDKSwift.build/Release-iphoneos/JDCloudSDKCore.build/Objects-normal/armv7/JDCloudSDKCore_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/lishijun1/Documents/swiftworkspace/JDCloudSDKSwift/jdcloud-sdk-swift-github/JDCloudSDKSwift/build/JDCloudSDKSwift.build/Release-iphoneos/JDCloudSDKCore.build/DerivedSources/JDCloudSDKCore_vers.c
